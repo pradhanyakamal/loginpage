e2a336098556a4ed00579a0f9e7f1e2ba960cdbc
Pradhanya Kamal <88843554+pradhanyakamal@users.noreply.github.com>
2024-07-23 19:02:13 +0530

    first commit
    basic login and register page using asp.net and sql server.

---
+++ Migrations/20240722111333_SecondMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace loginrazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "45ad14e1-96d9-4bd9-8997-832c8781727d", null, "teamleader", "teamleader" },
+                    { "c61b9116-0afd-43c3-8bbc-04436a50bb56", null, "developer", "developer" },
+                    { "e444b6d4-5d2f-4d66-a78c-7893607ba6d4", null, "admin", "admin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "45ad14e1-96d9-4bd9-8997-832c8781727d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c61b9116-0afd-43c3-8bbc-04436a50bb56");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e444b6d4-5d2f-4d66-a78c-7893607ba6d4");
+        }
+    }
+}
---
+++ Services/ApplicationDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using loginrazor.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace loginrazor.Services
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions options): base(options)
+        {
+                        
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var teamleader = new IdentityRole("teamleader");
+            teamleader.NormalizedName = "teamleader";
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var developer = new IdentityRole("developer");
+            developer.NormalizedName = "developer";
+
+            builder.Entity<IdentityRole>().HasData(admin, teamleader, developer);
+        }
+    }
+}
